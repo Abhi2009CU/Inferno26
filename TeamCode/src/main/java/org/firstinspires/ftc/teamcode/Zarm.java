3c2e09e57a46ee8177c68106a6f7a1364f03e74d
Abhinav Suresh <146779945+Abhi2009CU@users.noreply.github.com>
2025-08-30 10:51:16 -0700

    .
    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zarm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zarm.java
@@ -4,12 +4,19 @@ import com.arcrobotics.ftclib.controller.PIDController;
 
 public class Zarm {
     private Motor slideMotor;
+    
+    slideMotor.setRunMode(Motor.RunMode.RawPower);
+    slideMotor.resetEncoder();
+    
     public double MAX_POS, MIN_POS;
     private PIDController SLIDE_PID;
+    
     PIDController pidController = new PIDController(0.2,0.1, 0.1);
+    
     public void setPos(double p){
         slideMotor.set(Math.max(MIN_POS, Math.min(MAX_POS, p)));
     }
+    
     public void tickPID(){
         slideMotor.set(pidController.calculate(slideMotor.encoder.getPosition()));
     }
@@ -17,3 +24,4 @@ public class Zarm {
 
 
 }
+
